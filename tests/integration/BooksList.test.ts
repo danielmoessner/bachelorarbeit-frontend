5681e774f5a9ccad0d3185ef872a7e265f7351b6
danielmoessner <github@moesibert.de>
2021-12-01 14:21:30 +0100

    mock test

--- tests/integration/BooksList.test.ts
+++ tests/integration/BooksList.test.ts
@@ -1,7 +1,28 @@
 import { mount } from "@vue/test-utils";
 import BooksList from "@/components/BooksList.vue";
+import $axios from "@/axios";
+import MockAdapter from "axios-mock-adapter";
 
 test("books render properly", (done) => {
+  if (process.env.VITE_MOCK) {
+    const mock = new MockAdapter($axios);
+
+    const books = [
+      {
+        name: "1984",
+        author: "George Orwell",
+        published: 1949,
+      },
+      {
+        name: "The Gulag Archipelago",
+        author: "Aleksandr Solzhenitsyn",
+        published: 2018,
+      },
+    ];
+
+    mock.onGet("").reply(200, books);
+  }
+
   const component = mount(BooksList, {});
 
   setTimeout(() => {
